a3406dbfc96f6e3db0a435b581d498f291071fa9
Naveen Kumar <134830093+nvnKr09@users.noreply.github.com>
2023-10-22 23:56:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const enterButton = document.getElementById("enterBtn");
+        const statusParagraph = document.getElementById("status");
+        const metaverseStatus = document.getElementById("h1-status");
+
+        enterButton.addEventListener("click", function() {
+            // hiding the paragraph
+            statusParagraph.style.display = "none";
+            
+            // Displaying the h1 element
+            metaverseStatus.style.display = "block";
+        });
