4d52e763e886337c8175cd7fe4177be47779d928
Naveen Kumar <134830093+nvnKr09@users.noreply.github.com>
2023-10-23 23:30:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 //your JS code here. If required.
 const enterButton = document.getElementById("enterBtn");
 const status = document.getElementById("status");
-const h1 = document.getElementById("h1");
 
 enterButton.addEventListener("click", function() {
-    status.textContent = "Entered Metaverse";
-	h1.appendChild(status);
+    status.innerHTML = '<h1>Entered Metaverse</h1>';
 });
